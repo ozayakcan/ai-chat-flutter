b7c428d258e347df3d2500472d993cedffa75b3d
Ozay Akcan <mail@ozayakcan.com.tr>
2023-09-01 14:54:27 +0300

    HomePage seperated from main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-import 'package:aichat/utils/date.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import 'models/message.dart';
+import 'pages/home.dart';
 import 'utils/theme.dart';
-import 'widgets/message.dart';
 
 void main() {
   runApp(const MyApp());
@@ -49,116 +47,6 @@ class MyApp extends StatelessWidget {
           home: const MyHomePage(),
         );
       }),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  TextEditingController textEditingController = TextEditingController();
-  FocusNode focusNode = FocusNode();
-
-  ScrollController scrollController = ScrollController();
-
-  List<MessageModel> messages = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("AI Chat"),
-      ),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Expanded(
-              child: ListView(
-                reverse: true,
-                controller: scrollController,
-                children: [
-                  for (int i = messages.length - 1; i >= 0; i--)
-                    Flexible(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          if (i > 0)
-                            if (getDateString(context, messages[i].date) !=
-                                getDateString(context, messages[i - 1].date))
-                              MessageDateWidget(date: messages[i].date),
-                          if (i == 0) MessageDateWidget(date: messages[i].date),
-                          MessageWidget(
-                            messageModel: messages[i],
-                          ),
-                        ],
-                      ),
-                    )
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10, right: 5, left: 5),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      focusNode: focusNode,
-                      autofocus: true,
-                      controller: textEditingController,
-                      decoration: InputDecoration(
-                        hintText: AppLocalizations.of(context).type_something,
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (textEditingController.text.isNotEmpty) {
-                        setState(() {
-                          messages.add(
-                            MessageModel(
-                              message: textEditingController.text,
-                              isSender: true,
-                            ),
-                          );
-
-                          messages.add(
-                            MessageModel(
-                              message: textEditingController.text,
-                              isSender: false,
-                            ),
-                          );
-                        });
-                        textEditingController.text = "";
-                        focusNode.requestFocus();
-                        scrollController.animateTo(
-                          0.0,
-                          curve: Curves.easeOut,
-                          duration: const Duration(milliseconds: 300),
-                        );
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: const CircleBorder(),
-                      padding: const EdgeInsets.all(20),
-                      backgroundColor: Colors.blue,
-                      foregroundColor: Colors.white,
-                    ),
-                    child: const Icon(Icons.send),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,116 @@
+import 'package:aichat/utils/date.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../models/message.dart';
+import '../widgets/message.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController textEditingController = TextEditingController();
+  FocusNode focusNode = FocusNode();
+
+  ScrollController scrollController = ScrollController();
+
+  List<MessageModel> messages = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("AI Chat"),
+      ),
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(
+              child: ListView(
+                reverse: true,
+                controller: scrollController,
+                children: [
+                  for (int i = messages.length - 1; i >= 0; i--)
+                    Flexible(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          if (i > 0)
+                            if (getDateString(context, messages[i].date) !=
+                                getDateString(context, messages[i - 1].date))
+                              MessageDateWidget(date: messages[i].date),
+                          if (i == 0) MessageDateWidget(date: messages[i].date),
+                          MessageWidget(
+                            messageModel: messages[i],
+                          ),
+                        ],
+                      ),
+                    )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10, right: 5, left: 5),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      focusNode: focusNode,
+                      autofocus: true,
+                      controller: textEditingController,
+                      decoration: InputDecoration(
+                        hintText: AppLocalizations.of(context).type_something,
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (textEditingController.text.isNotEmpty) {
+                        setState(() {
+                          messages.add(
+                            MessageModel(
+                              message: textEditingController.text,
+                              isSender: true,
+                            ),
+                          );
+
+                          messages.add(
+                            MessageModel(
+                              message: textEditingController.text,
+                              isSender: false,
+                            ),
+                          );
+                        });
+                        textEditingController.text = "";
+                        focusNode.requestFocus();
+                        scrollController.animateTo(
+                          0.0,
+                          curve: Curves.easeOut,
+                          duration: const Duration(milliseconds: 300),
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: const CircleBorder(),
+                      padding: const EdgeInsets.all(20),
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Icon(Icons.send),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
